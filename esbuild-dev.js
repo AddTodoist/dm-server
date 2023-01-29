46e007d4de0f835e5dc5c6e82e3fcef2d4e78695
dubisdev <dubisdev@gmail.com>
2023-01-29 18:38:46 +0100

    chore: update esbuild

--- esbuild-dev.js
+++ esbuild-dev.js
@@ -1,24 +1,21 @@
-import esbuild from 'esbuild';
+import { context } from 'esbuild';
 import { typecheckPlugin } from '@jgoz/esbuild-plugin-typecheck';
 
 import fs from 'fs';
 const json = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 let deps = Object.keys(json.dependencies);
 
-esbuild
-  .build({
-    watch: true,
-    platform: 'node',
-    logLevel: 'info',
-    entryPoints: [ 'src/index.ts' ],
-    bundle: true,
-    minify: false,
-    format: 'esm',
-    target: 'node16',
-    outdir: 'dist',
-    external: deps,
-    plugins: [typecheckPlugin({
-      watch: true
-    })]
-  })
-  .catch(() => process.exit(1));
+context({
+  platform: 'node',
+  logLevel: 'info',
+  entryPoints: ['src/index.ts'],
+  bundle: true,
+  minify: false,
+  format: 'esm',
+  target: 'node16',
+  outdir: 'dist',
+  external: deps,
+  plugins: [typecheckPlugin({
+    watch: true
+  })]
+}).then(ctx => ctx.watch().catch(() => process.exit(1)));
--- esbuild.js
+++ esbuild.js
@@ -1,20 +1,19 @@
-import esbuild from 'esbuild';
+import { build } from 'esbuild';
 import fs from 'fs';
 const json = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 let deps = Object.keys(json.dependencies);
 
 const entryPoint = 'src/index.ts';
 
-esbuild
-  .build({
-    platform: 'node',
-    logLevel: 'info',
-    entryPoints: [entryPoint],
-    bundle: true,
-    minify: true,
-    format: 'esm',
-    target: 'node16',
-    outfile: 'dist/index.js',
-    external: deps,
-  })
+build({
+  platform: 'node',
+  logLevel: 'info',
+  entryPoints: [entryPoint],
+  bundle: true,
+  minify: true,
+  format: 'esm',
+  target: 'node16',
+  outfile: 'dist/index.js',
+  external: deps,
+})
   .catch(() => process.exit(1));
